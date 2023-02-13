062a61879b50c5cfda9030947284f10371bfaf1f
Brian Lee <brianlee1597@gmail.com>
2023-02-12 23:11:53 -0500

    commit changes

---
+++ project2.js
@@ -0,0 +1,5 @@
+document.getElementById("sign-up-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you!  Please watch your email for our exciting newsletter and offers!");
+  location.reload();
+})
